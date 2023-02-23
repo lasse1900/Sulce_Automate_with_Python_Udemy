c20bfad5d0dc00ccedd831d37aaf69eb7a2561ad
Lauri Kyttälä <lauri.kyttala@etteplan.com>
2023-02-23 14:52:56 +0200

    added Section6

---
+++ Section6/email_once_a_day.py
@@ -0,0 +1,28 @@
+import yagmail
+import os
+from dotenv import load_dotenv
+load_dotenv()
+token = os.environ.get("YAGMAIL_PASS")
+import time
+from datetime import datetime as dt
+# https://www.nylas.com/blog/making-use-of-environment-variables-in-python/
+# https://www.pythonanywhere.com/
+
+sender = 'lauri.kyttala@gmail.com'
+receiver = 'lauri.kyttala@etteplan.com'
+
+subject = "This is the subject!"
+
+
+contents = """
+Here is the content of the email! 
+Hi!
+"""
+
+while True:
+  now = dt.now()
+  if now.hour == 14 and now.minute == 46:
+    yag = yagmail.SMTP(user=sender, password=token)
+    yag.send(to=receiver, subject=subject, contents=contents)
+    print("Email Sent!")
+    time.sleep(60)
---
+++ Section6/single_email.py
@@ -0,0 +1,22 @@
+import yagmail
+import os
+from dotenv import load_dotenv
+load_dotenv()
+token = os.environ.get("YAGMAIL_PASS")
+# https://www.nylas.com/blog/making-use-of-environment-variables-in-python/
+# https://www.pythonanywhere.com/
+
+sender = 'lauri.kyttala@gmail.com'
+receiver = 'lauri.kyttala@etteplan.com'
+
+subject = "This is the subject!"
+
+
+contents = """
+Here is the content of the email! 
+Hi!
+"""
+
+yag = yagmail.SMTP(user=sender, password=token)
+yag.send(to=receiver, subject=subject, contents=contents)
+print("Email Sent!")
